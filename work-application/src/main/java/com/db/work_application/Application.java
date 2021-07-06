65c05a8ec3c318a654280956c72dad8c75e79d7c
Artem Galushko <galushko@my.com>
2021-07-06 15:19:14 +0300

    small fixes

--- work-application/src/main/java/com/db/work_application/Application.java
+++ work-application/src/main/java/com/db/work_application/Application.java
@@ -1,11 +1,17 @@
 package com.db.work_application;
 
 import com.db.work_application.config.JavaConfig;
+import com.db.work_application.controller.ClientController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        ClientController controller = context.getBean(ClientController.class);
+        controller.createClient(1,"Petya", "Ivanov");
+        controller.createClient(2,"Masha", "Ivanova");
+        System.out.println(controller.findById(1));
+        System.out.println(controller.getAll());
     }
 }
--- work-application/src/main/java/com/db/work_application/model/Client.java
+++ work-application/src/main/java/com/db/work_application/model/Client.java
@@ -1,37 +1,21 @@
 package com.db.work_application.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Client {
     private long id;
     private String firstName;
     private String lastName;
 
-    public Client(long id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    @Override
+    public String toString() {
+        return "Client id: " + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'';
     }
 }
