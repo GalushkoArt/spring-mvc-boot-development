edecf35e096f8d6ed87585bcb7eba49e5ff5ea93
Artem Galushko <galushko@my.com>
2021-07-18 16:26:08 +0300

    add validations

--- work-application/src/main/java/com/db/work_application/controller/ClientController.java
+++ work-application/src/main/java/com/db/work_application/controller/ClientController.java
@@ -3,12 +3,17 @@ package com.db.work_application.controller;
 import com.db.work_application.model.Client;
 import com.db.work_application.service.ClientService;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/clients")
+@Validated
 public class ClientController {
     private final ClientService service;
 
@@ -17,7 +22,7 @@ public class ClientController {
     }
 
     @GetMapping("/{id}")
-    public Client findById(@PathVariable long id) {
+    public Client findById(@PathVariable @Positive long id) {
         Client client = service.findById(id);
         if (client == null) throw new ClientNotFoundException(id);
         return client;
@@ -30,12 +35,12 @@ public class ClientController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Client createClient(@RequestBody Client client) {
+    public Client createClient(@RequestBody @Valid Client client) {
         return service.createClient(client);
     }
 
     @GetMapping("/get-by-first-name-last-name/{firstName}/{lastName}")
-    public List<Client> getClientsByFirstNameAndLastName(@PathVariable String firstName, @PathVariable String lastName) {
+    public List<Client> getClientsByFirstNameAndLastName(@PathVariable @NotEmpty String firstName, @PathVariable @NotEmpty String lastName) {
         return service.getClientsByFirstNameAndLastName(firstName, lastName);
     }
 }
--- work-application/src/main/java/com/db/work_application/model/Client.java
+++ work-application/src/main/java/com/db/work_application/model/Client.java
@@ -1,6 +1,10 @@
 package com.db.work_application.model;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 import static com.db.work_application.utils.ObjectUtil.asJsonString;
 
@@ -10,8 +14,13 @@ import static com.db.work_application.utils.ObjectUtil.asJsonString;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Client {
+    @Positive
     private long id;
+    @NotNull
+    @Length(min = 1, max = 60)
     private String firstName;
+    @NotNull
+    @Length(min = 1, max = 60)
     private String lastName;
 
     @Override
